a21f9210e33f0e22a83c01e50429652f7d9b428b
Ahalya Das <ahalyadas01@gmail.com>
2021-05-09 19:34:07 +0530

    Changed browser icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-//import logo from './cisco_logo.png';
-//import sidebar from './sidebar.png';
+import logo from './cisco_logo_white.png';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login/Login';
@@ -15,10 +14,22 @@ class App extends Component {
   handleChange = (newValue) => {
     this.setState({ user: newValue });
   }
+
+  openNav=()=>{
+    document.getElementById("mySidenav").style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  closeNav=()=> {
+    document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
   
   
   render() {
-    if(!this.state.user) {
+    if(this.state.user) { //Make it ! to enable login
       return(
           <Login user={this.state.user} onChange={this.handleChange}/>
       );
@@ -26,17 +37,28 @@ class App extends Component {
     else {
       return (
         <div className="wrapper">
-          <div className="pageHeader">
+          <div className="App-header">
+            <span id="App-sidebar" onClick={this.openNav} alt ="sidebar"> &#9776;</span>
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className= "App-header-text">DENEPS</div>
+            <div id="mySidenav" class="sidenav">
+              {//<a href="#" class="closebtn" onClick={this.closeNav}>&times;</a>
+              }
+              <a href="/dashboard">Home</a>
+              <a href="/points">Points</a>
+              <a href="#">Requests</a>
+              <a href="#">Admin</a>
+          </div>
           </div>
           <BrowserRouter>
             <Switch>
-            <Route path="/">
+            <Route exact path="/">
                 <Dashboard />
               </Route>
-              <Route path="/dashboard">
+              <Route path="/dashboard" component={Dashboard}>
                 <Dashboard />
               </Route>
-              <Route path="/points">
+              <Route path="/points" component={Points}>
                 <Points />
               </Route>
             </Switch>
@@ -44,22 +66,6 @@ class App extends Component {
         </div>
       );
     }
-    
-
-    /*return (
-      <div className="App">
-        <header className="App-header">
-          <img src={sidebar} className="App-side-bar" alt ="sidebar"/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className= "App-header-text">DENEPS</div>
-        </header>
-        <body className="App-data">
-        <p className= "App-intro">
-            {this.state.apiResponse}
-          </p>
-        </body>
-      </div>
-    );*/
   }
 }
 
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,31 +2,12 @@ import React from 'react';
 import "./Dashboard.css";
 
 export default function Dashboard() {
-  function openNav(){
-    document.getElementById("mySidenav").style.width = "250px";
-    //document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    //document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-  }
   return(
     <div>
-      <div id="mySidenav" class="sidenav">
-        <a href="#" class="closebtn" onClick={closeNav}>&times;</a>
-        <a href="#">Home</a>
-        <a href="#">Points</a>
-        <a href="#">Requests</a>
-        <a href="#">Admin</a>
-      </div>
+      
   <div id="main">
     <h2>Dashboard</h2>
     <p>DATA to be Added</p>
-    <span style={{"font-size":'30px','cursor':'pointer'}} onClick={openNav}> &#9776; open</span>
   </div>
     </div>
     
